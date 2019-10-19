fd9f42873edae376e37d27f6db5df39a980a26b5
Rick <riccardoforteleoni@icloud.com>
2019-10-19 07:50:58 +0200

    firsttest

--- shop.py
+++ shop.py
@@ -1,3 +1,6 @@
 class Shop:
-    def __init__(self,address,a,b,c,d,e,f):
+    def __init__(self,address,open_at,close_at,d,e,f):
         self.address = address
+        self.open_hour = open_at
+        self.close_hour = close_at
+        
